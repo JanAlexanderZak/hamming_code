80f3d567c41283996e37f8ce8a68208ee75c05e4
JanAlexanderZak <janalexzak@gmail.com>
2020-11-30 16:48:05 +0100

    refactor: parity_check function

--- src/hamming_code.py
+++ src/hamming_code.py
@@ -3,17 +3,26 @@ from functools import reduce
 np.random.seed(7)
 
 
-def send_bits(_array):
-    random_index = np.random.randint(0, _array.shape[0])  # 15
-    if _array[random_index] == 1:
-        _array[random_index] = 0
+def send_bits(_bits: np.array):
+    """ Alters a bit at a random position
+    :param _bits: _bits where one bit is to be altered
+    :return: altered list of bits
+    """
+    random_index: int = np.random.randint(0, _bits.shape[0])  # 15
+    if _bits[random_index] == 1:
+        _bits[random_index] = 0
     else:
-        _array[random_index] = 1
-    return _array
+        _bits[random_index] = 1
+    return _bits
 
 
-def prepare_bits(_bits):
-    error_bit = reduce(lambda x, y: x ^ y, [i for i, bit in enumerate(_bits) if bit])
+def prepare_bits(_bits: np.array):
+    """ Alters list of bits that the parity check is 0
+    :param _bits:
+    :return:
+    """
+    error_bit = parity_check(_bits)
+    print("Parity check before preperation: ", error_bit)
     error_bit = bin(error_bit)[2:].rjust(4, '0')
 
     # Get integer index
@@ -25,8 +34,12 @@ def prepare_bits(_bits):
     _bits[column] = not _bits[column]
 
     # Failcheck
-    # error_bit = reduce(lambda x, y: x ^ y, [i for i, bit in enumerate(_bits) if bit])
+    print("Parity check after preperation: ", parity_check(_bits))
     return _bits
+
+
+def parity_check(_bits):
+    return reduce(lambda x, y: x ^ y, [i for i, bit in enumerate(_bits) if bit])
 
 
 if __name__ == '__main__':
@@ -39,5 +52,5 @@ if __name__ == '__main__':
     send_bits_ = send_bits(prepared_bits)  # alters at index=15, last element
     print(send_bits_)
     print("Matrix representation: \n", send_bits_.reshape(4, 4))
-    x = reduce(lambda x, y: x ^ y, [i for i, bit in enumerate(send_bits_) if bit])
+    x = parity_check(send_bits_)
     print(x)
